da08ae44a7ddd3e44957a29addf9d31463250b31
kategomezny <35971954+kategomezny@users.noreply.github.com>
2018-04-15 17:21:31 -0400

    Create task_02.py

---
+++ task_02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Accesing data from dictionaries"""
+
+
+import data
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+
+BAND_NAMES = data.BANDS.keys()
